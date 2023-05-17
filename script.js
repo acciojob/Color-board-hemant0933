1f371608c25794da89869bce29df3fffc6758cdf
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-05-17 11:32:02 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+window.addEventListener('DOMContentLoaded', function() {
+    const board = document.getElementById('board');
+  
+    for (let i = 0; i < 800; i++) {
+      const square = document.createElement('div');
+      square.className = 'square';
+      board.appendChild(square);
+  
+      square.addEventListener('mouseover', function() {
+        square.style.backgroundColor = getRandomColor();
+      });
+  
+      square.addEventListener('mouseout', function() {
+        setTimeout(function() {
+          square.style.backgroundColor = '#ccc';
+        }, 1000);
+      });
+    }
+  });
+  
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+  
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+  
+    return color;
+  }
+  
